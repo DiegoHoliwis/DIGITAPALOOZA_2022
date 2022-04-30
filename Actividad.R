53c5ad867b9183fb3f0b0f30ad9d9ad263772387
Diego Muñoz <dimunozu@gmail.com>
2022-04-30 13:29:45 -0400

    .

--- Actividad.R
+++ Actividad.R
@@ -4,6 +4,10 @@ library(rvest)
 
 pagina <- read_html('https://www.mercadolibre.cl/ofertas?promotion_type=deal_of_the_day&container_id=MLC779365-1&page=1')
 
-pagina %>% 
-  html_element(xpath = '//p[@class = "promotion-item__title"]') %>% 
+Nombres <- pagina %>% 
+  html_elements(xpath = '//p[@class = "promotion-item__title"]') %>% 
+  html_text2()
+
+precio <- pagina %>% 
+  html_elements(xpath = '//span[@class = "promotion-item__price"]') %>% 
   html_text2()
