e3405d75e7325654f31689cd21a785efe7b6d5a3
Diego Muñoz <dimunozu@gmail.com>
2022-04-30 13:18:49 -0400

    .

--- Actividad.R
+++ Actividad.R
@@ -1,2 +1,9 @@
 library(tidyverse)
 library(rvest)
+
+
+pagina <- read_html('https://www.mercadolibre.cl/ofertas?promotion_type=deal_of_the_day&container_id=MLC779365-1&page=1')
+
+pagina %>% 
+  html_element(xpath = '//p[@class = "promotion-item__title"]') %>% 
+  html_text2()
